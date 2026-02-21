241947110a4258694386302ece6afbacccb8ee44
shaishab316 <shaishabchandrashil@gmail.com>
2026-02-21 14:26:59 +0600

    feat: initialize new TypeScript project with Express server, basic configuration, and family tree interfaces.

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export default {
+  port: Number(process.env.PORT ?? 3000),
+  neo4j: {
+    uri: String(process.env.NEO4J_URI),
+    user: String(process.env.NEO4J_USER),
+    password: String(process.env.NEO4J_PASSWORD),
+    database: String(process.env.NEO4J_DATABASE),
+  }
+}
---
+++ src/modules/family_tree/family_tree.interface.ts
@@ -0,0 +1,104 @@
+/**
+ *  UUID is a universally unique identifier.
+ */
+export type UUID = string;
+
+/**
+ * Gender is a type that can be 'MALE', 'FEMALE', or 'OTHER'.
+ */
+export type Gender = 'MALE' | 'FEMALE' | 'OTHER';
+
+/**
+ * Person is a person in the family tree.
+ */
+export interface Person {
+	created_at: Date;
+	updated_at: Date;
+
+	/** primary key of the person */
+	person_id: UUID;
+
+	/**
+	 * name used to display the person
+	 */
+	display_name: string;
+	first_name?: string;
+	last_name?: string;
+	middle_name?: string;
+	name_suffix?: string;
+	name_prefix?: string;
+
+	gender?: Gender;
+
+	/** date of birth */
+	dob?: Date;
+	/** date of death */
+	dod?: Date;
+
+	/** flag to indicate if the person is alive */
+	is_alive?: boolean;
+}
+
+/**
+ * Relationship kind is descriptive of the relationship between two people.
+ *
+ * - PARENT_OF: from -> to is parent -> child
+ * - SPOUSE_OF: from -> to is spouse -> spouse
+ */
+export type RelationshipKind = 'PARENT_OF' | 'SPOUSE_OF';
+
+/**
+ * Parentage kind is descriptive of the relationship between a parent and child.
+ *
+ * - BIOLOGICAL: biological parent
+ * - ADOPTIVE: adoptive parent
+ * - STEP: step parent
+ * - FOSTER: foster parent
+ * - GUARDIAN: guardian
+ */
+export type ParentageKind =
+	| 'BIOLOGICAL'
+	| 'ADOPTIVE'
+	| 'STEP'
+	| 'FOSTER'
+	| 'GUARDIAN';
+
+/**
+ * Marriage status is descriptive of the marriage status of a spouse.
+ *
+ * - MARRIED: married
+ * - WIDOWED: widowed
+ * - DIVORCED: divorced
+ * - SEPARATED: separated
+ * - NEVER_MARRIED: never married
+ */
+export type MarriageStatus = 'MARRIED' | 'WIDOWED' | 'DIVORCED' | 'SEPARATED';
+
+/**
+ * Person relation is a directed edge between two people.
+ */
+export interface PersonRelation {
+	created_at: Date;
+	updated_at: Date;
+
+	/** primary key of the relation */
+	relation_id: UUID;
+
+	/** Edge direction: from -> to */
+	from_person_id: UUID;
+	to_person_id: UUID;
+
+	/** kind of relationship */
+	relationship_kind: RelationshipKind;
+
+	/** only for relationship_kind === RelationshipKind.PARENT_OF */
+	parentage_kind?: ParentageKind;
+
+	/** only for relationship_kind === RelationshipKind.SPOUSE_OF */
+	marriage_status?: MarriageStatus;
+
+	/** start date of the relationship */
+	start_date?: Date;
+	/** end date of the relationship */
+	end_date?: Date;
+}
